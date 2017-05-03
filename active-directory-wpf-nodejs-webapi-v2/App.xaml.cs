9bacf5893d65f52b4449a1dc67c76528db979bf2
Jean-Marc Prieur <jmprieur@microsoft.com>
2017-05-03 11:03:42 +0200

    Initial commit of this sample produced from https://github.com/Microsoft/azure-docs/blob/master/articles/active-directory/develop/GuidedSetups/MobileAndDesktopApp/active-directory-mobileanddesktopapp-windowsdesktop-intro.md

---
+++ active-directory-wpf-nodejs-webapi-v2/App.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using Microsoft.Identity.Client;
+
+namespace active_directory_wpf_nodejs_webapi_v2
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        //Below is the clientId of your app registration. 
+        //You have to replace the below with the Application Id for your app registration
+        private static string ClientId = "0b8b0665-bc13-4fdc-bd72-e0227b9fc011";
+
+        private static PublicClientApplication _clientApp = new PublicClientApplication(ClientId);
+
+        public static PublicClientApplication PublicClientApp { get { return _clientApp; } }
+    }
+}
---
+++ active-directory-wpf-nodejs-webapi-v2/MainWindow.xaml.cs
@@ -0,0 +1,139 @@
+﻿using Microsoft.Identity.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace active_directory_wpf_nodejs_webapi_v2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        //Set the API Endpoint to Graph 'me' endpoint
+        string graphAPIEndpoint = "https://graph.microsoft.com/v1.0/me";
+
+        //Set the scope for API call to user.read
+        string[] scopes = new string[] { "user.read" };
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Call AcquireTokenAsync - to acquire a token requiring user to sign-in
+        /// </summary>
+        private async void CallGraphButton_Click(object sender, RoutedEventArgs e)
+        {
+            AuthenticationResult authResult = null;
+
+            try
+            {
+                if (authResult == null)
+                {
+                    authResult = await App.PublicClientApp.AcquireTokenSilentAsync(scopes, App.PublicClientApp.Users.FirstOrDefault());
+                }
+            }
+            catch (MsalUiRequiredException ex)
+            {
+                // A MsalUiRequiredException happened on AcquireTokenSilentAsync. This indicates you need to call AcquireTokenAsync to acquire a token
+                System.Diagnostics.Debug.WriteLine($"MsalUiRequiredException: {ex.Message}");
+
+                try
+                {
+                    authResult = await App.PublicClientApp.AcquireTokenAsync(scopes);
+                }
+                catch (MsalException msalex)
+                {
+                    ResultText.Text = $"Error Acquiring Token:{System.Environment.NewLine}{msalex}";
+                }
+            }
+            catch (Exception ex)
+            {
+                ResultText.Text = $"Error Acquiring Token Silently:{System.Environment.NewLine}{ex}";
+                return;
+            }
+
+            if (authResult != null)
+            {
+                ResultText.Text = await GetHttpContentWithToken(graphAPIEndpoint, authResult.AccessToken);
+                DisplayBasicTokenInfo(authResult);
+                this.SignOutButton.Visibility = Visibility.Visible;
+            }
+        }
+
+        /// <summary>
+        /// Perform an HTTP GET request to a URL using an HTTP Authorization header
+        /// </summary>
+        /// <param name="url">The URL</param>
+        /// <param name="token">The token</param>
+        /// <returns>String containing the results of the GET operation</returns>
+        public async Task<string> GetHttpContentWithToken(string url, string token)
+        {
+            var httpClient = new System.Net.Http.HttpClient();
+            System.Net.Http.HttpResponseMessage response;
+            try
+            {
+                var request = new System.Net.Http.HttpRequestMessage(System.Net.Http.HttpMethod.Get, url);
+                //Add the token in Authorization header
+                request.Headers.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", token);
+                response = await httpClient.SendAsync(request);
+                var content = await response.Content.ReadAsStringAsync();
+                return content;
+            }
+            catch (Exception ex)
+            {
+                return ex.ToString();
+            }
+        }
+
+        /// <summary>
+        /// Sign out the current user
+        /// </summary>
+        private void SignOutButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (App.PublicClientApp.Users.Any())
+            {
+                try
+                {
+                    App.PublicClientApp.Remove(App.PublicClientApp.Users.FirstOrDefault());
+                    this.ResultText.Text = "User has signed-out";
+                    this.CallGraphButton.Visibility = Visibility.Visible;
+                    this.SignOutButton.Visibility = Visibility.Collapsed;
+                }
+                catch (MsalException ex)
+                {
+                    ResultText.Text = $"Error signing-out user: {ex.Message}";
+                }
+            }
+        }
+
+        /// <summary>
+        /// Display basic information contained in the token
+        /// </summary>
+        private void DisplayBasicTokenInfo(AuthenticationResult authResult)
+        {
+            TokenInfoText.Text = "";
+            if (authResult != null)
+            {
+                TokenInfoText.Text += $"Name: {authResult.User.Name}" + Environment.NewLine;
+                TokenInfoText.Text += $"Username: {authResult.User.DisplayableId}" + Environment.NewLine;
+                TokenInfoText.Text += $"Token Expires: {authResult.ExpiresOn.ToLocalTime()}" + Environment.NewLine;
+                TokenInfoText.Text += $"Access Token: {authResult.AccessToken}" + Environment.NewLine;
+            }
+        }
+    }
+}

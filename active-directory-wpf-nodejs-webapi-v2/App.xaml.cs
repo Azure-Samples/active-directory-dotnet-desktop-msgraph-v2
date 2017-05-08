716863f7ba0eb5f6d02b1dc764a6445711024a6b
Jean-Marc Prieur <jmprieur@microsoft.com>
2017-05-08 17:27:35 +0200

    - Upgrading the version of msal.net - Adding a Token cache so that the user does no longer need to sign-in he's already signed-in and the token has not expired.

--- active-directory-wpf-nodejs-webapi-v2/App.xaml.cs
+++ active-directory-wpf-nodejs-webapi-v2/App.xaml.cs
@@ -14,11 +14,15 @@ namespace active_directory_wpf_msgraph_v2
     /// </summary>
     public partial class App : Application
     {
+        static App()
+        {
+            _clientApp = new PublicClientApplication(ClientId, "https://login.microsoftonline.com/common", TokenCacheHelper.GetUserCache());
+        }
         //Below is the clientId of your app registration. 
         //You have to replace the below with the Application Id for your app registration
         private static string ClientId = "0b8b0665-bc13-4fdc-bd72-e0227b9fc011";
 
-        private static PublicClientApplication _clientApp = new PublicClientApplication(ClientId);
+        private static PublicClientApplication _clientApp ;
 
         public static PublicClientApplication PublicClientApp { get { return _clientApp; } }
     }

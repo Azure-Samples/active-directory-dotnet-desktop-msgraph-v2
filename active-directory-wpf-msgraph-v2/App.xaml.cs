41af158070b4767a056f610ca619109fa374485b
Jean-Marc Prieur <jmprieur@microsoft.com>
2018-10-31 23:23:54 +0100

    Improving based on feedback from Andre

--- active-directory-wpf-msgraph-v2/App.xaml.cs
+++ active-directory-wpf-msgraph-v2/App.xaml.cs
@@ -16,20 +16,20 @@ namespace active_directory_wpf_msgraph_v2
     {
         static App()
         {
-            // Tools like the application creation portal replace the value of Tenant by the tenant ID, or common, or organizations 
-            // depending on the audience of the created application. But we still want this sample to be usable as is, so if this 
-            // replacement did not happen, we'd want to enable sign-in with any Work & School or Microsoft Personal account (commmon)
-            string tenant = (Tenant == "TenantId") ? "common" : Tenant;
-            string authority = $"https://login.microsoft.online.com/{tenant}";
-            _clientApp = new PublicClientApplication(ClientId, "https://login.microsoftonline.com/common", TokenCacheHelper.GetUserCache());
+            string authority = $"https://login.microsoftonline.com/{Tenant}";
+            _clientApp = new PublicClientApplication(ClientId, authority, TokenCacheHelper.GetUserCache());
         }
 
-        // Below is the clientId (Application Id) of your app registration. 
-        // You have to replace the below with the Application Id for your app registration
+        // Below are the clientId (Application Id) of your app registration and the tenant information. 
+        // You have to replace:
+        // - the content of ClientID with the Application Id for your app registration
+        // - Te content of Tenant by the information about the accounts allowed to sign-in in your application:
+        //   - For Work or School account in your org, use your tenant ID, or domain
+        //   - for any Work or School accounts, use organizations
+        //   - for any Work or School acounts, or Microsoft personal account, use common
+        //   - for Microsoft Personal account, use consumers
         private static string ClientId = "0b8b0665-bc13-4fdc-bd72-e0227b9fc011";
-
-        // Tenant(s) allowed to sign-in users in the application.
-        private static string Tenant = "TenantId";
+        private static string Tenant = "common";
 
         private static PublicClientApplication _clientApp ;
 
